9dc55c1640a4da60bdce6c06c157df7b5a7d2c95
AltLight <realistically.paranoid@gmail.com>
2020-10-22 09:57:01 -0500

    updating get-imagedata module

--- Powershell Modules/Get-ImageData/Get-ImageData.psm1
+++ Powershell Modules/Get-ImageData/Get-ImageData.psm1
@@ -8,8 +8,18 @@ function Get-ImageData
     [string]$defaultDir = "N:\Pictures"
     [array]$picDirs = @()
     [array]$Images = @()
+    [array]$skippedItems = @()
     [int16]$dirLoopCounter = 0
     $objShell  = New-Object -ComObject Shell.Application
+    $StopWatch = [Diagnostics.StopWatch]::StartNew()
+    [array]$extensions = @(
+        ".jpg",
+        ".png",
+        ".jpeg",
+        ".webp",
+        ".gif"
+    )
+
     if (0 -lt $SourcePath.Length)
     {
         Write-Verbose "Testing user defined source path: $SourcePath"
@@ -21,9 +31,10 @@ function Get-ImageData
         Write-Verbose "User defined source path is valid."
         $defaultDir = $SourcePath
     }
-    if (!(Test-Path -Path $SourcePath))
+    if (!(Test-Path -Path $defaultDir))
     {
         Write-Error "The given image directory cannot be reached: $defaultDir`nAborting operations."
+        break
     }
 
     $duration =  measure-command {
@@ -35,6 +46,7 @@ function Get-ImageData
     }
     [int16]$dirCount = $picDirs.Count
     Write-Verbose "Found $dirCount folder(s) in $($Duration.Minutes):$($Duration.Seconds) mm:ss"
+
     foreach ($dir in $picDirs)
     {
         $dirLoopCounter++
@@ -44,16 +56,20 @@ function Get-ImageData
             -Status "Processing pictures in $dir" `
             -PercentComplete (($dirLoopCounter / ($dirCount + 1)) * 100)
         $objFolder = $objShell.namespace($dir)
+        $totalItems = ($objFolder.Items()).count
+        $itemCounter = 1
         foreach ($File in $objFolder.items())
         {
             Write-Progress `
                 -Id 2 `
                 -ParentId 1 `
-                -Activity "Getting image metadata for:" `
-                -Status "$($file.name)"
-            if ($objFolder.getDetailsOf($File, 156) -in $Extension)
+                -Activity "Item $itemCounter of $totalItems" `
+                -Status "Getting image metadata for: $($file.name)" `
+                -PercentComplete ($itemCounter / ($totalItems +1) * 100)
+
+            Write-Verbose "Processing file '$($File.Path)'"
+            if ($objFolder.getDetailsOf($File, 156) -in $extensions) 
             {
-                Write-Verbose "Processing file '$($File.Path)'"
                 $Props = [ordered]@{
                     Name          = $File.Name
                     FullName      = $File.Path
@@ -75,8 +91,16 @@ function Get-ImageData
                 $Images += New-Object `
                     -TypeName psobject `
                     -Property $Props
-            } # if $Extension
+            } # if $extensions
+            $itemCounter++
         } # foreach $File
+    } # foreach $dir
+    $StopWatch.Stop()
+    Write-Host "[INFO] Time to process all images: $($StopWatch.Elapsed)"
+    if (0 -lt $skippedItems.Count)
+    {
+        Write-Host "Skipped Files:`n"
+        $skippedItems | Format-Table -AutoSize -Wrap
     }
     return $Images
 }
